5178668ebd9297870b6e74fb815a0b1281d0557a
Filip Sedivy <mail@filipsedivy.cz>
2025-05-09 00:24:51 +0200

    Implements numpy

--- neuron/core/neuron.py
+++ neuron/core/neuron.py
@@ -1,33 +1,37 @@
-from ..utils.linalg import dot_product, scalar_multiply, vector_add
-from ..functions.activations import step_function
-
+import numpy as np
 import json
 
 
 class Neuron:
-    def __init__(self, input_size, activation=step_function):
-        self.weights = [0.0] * input_size
+    def __init__(self, input_size, activation=None):
+        if activation is None:
+            self.activation = lambda x: np.where(x >= 0, 1, 0)
+        else:
+            self.activation = activation
+
+        self.weights = np.zeros(input_size, dtype=np.float16)
         self.bias = 0.0
-        self.activation = activation
 
     def forward(self, inputs):
-        z = dot_product(self.weights, inputs) + self.bias
+        inputs = np.asarray(inputs)
+        z = np.dot(self.weights, inputs) + self.bias
         return self.activation(z)
 
     def update_weights(self, inputs, error, learning_rate):
-        delta = scalar_multiply(learning_rate * error, inputs)
-        self.weights = vector_add(self.weights, delta)
+        inputs = np.asarray(inputs)
+        delta = learning_rate * error * inputs
+        self.weights += delta
         self.bias += learning_rate * error
 
     def save_weights(self, filepath):
         with open(filepath, 'w') as f:
             json.dump({
-                'weights': self.weights,
-                'bias': self.bias
+                'weights': self.weights.tolist(),
+                'bias': float(self.bias)
             }, f)
 
     def load_weights(self, filepath):
         with open(filepath, 'r') as f:
             data = json.load(f)
-            self.weights = data['weights']
+            self.weights = np.array(data['weights'], dtype=np.float64)
             self.bias = data['bias']
